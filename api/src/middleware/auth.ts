8532f911a2018d1f91531f4f4937476848d0e879
Jerry <jerry.rausk@gmail.com>
2023-12-26 11:28:41 +0100

    fix

--- api/src/middleware/auth.ts
+++ api/src/middleware/auth.ts
@@ -3,19 +3,19 @@ import { Container, CosmosClient } from "@azure/cosmos";
 const TOKENHEADERNAME = "X-App-Authorization";
 const DBNAME = process.env.CosmosDBName;
 const CONTAINERNAME = process.env.CosmosContainerName;
-type User = {email: string, name: string}
+type User = { email: string, name: string }
 
 async function getUserInfoFromIssuer(token: string) {
     const USERINFOURL = process.env.TokenIssuerUrl;
     const EXPECTEDKEYS = ["email", "name"]
-    const headers = {"Authorization": token, "Content-Type": "application/json"}
-    const userInfoResponse =  await fetch(USERINFOURL, {headers});
-    if(userInfoResponse.status !== 200) {
+    const headers = { "Authorization": token, "Content-Type": "application/json" }
+    const userInfoResponse = await fetch(USERINFOURL, { headers });
+    if (userInfoResponse.status !== 200) {
         console.error(`Could not fetch userinfo from provider, status ${userInfoResponse.status}`);
         return null;
     }
     const userInfo = await userInfoResponse.json();
-    if(!EXPECTEDKEYS.every(key => Object.keys(userInfo).includes(key))) {
+    if (!EXPECTEDKEYS.every(key => Object.keys(userInfo).includes(key))) {
         console.error(`Details was missing from userinfo, userinfo: ${JSON.stringify(userInfo)}`);
         return null
     }
@@ -25,7 +25,7 @@ async function getUserInfoFromIssuer(token: string) {
 async function getCosmosClient() {
     const endpoint = process.env.CosmosUrl;
     const key = process.env.CosmosKey;
-    return new CosmosClient({endpoint, key})
+    return new CosmosClient({ endpoint, key })
 }
 
 function getContainer(client: CosmosClient) {
@@ -33,31 +33,40 @@ function getContainer(client: CosmosClient) {
     return db.container(CONTAINERNAME)
 }
 
-async function getItemOrNullById(container: Container, id: string) {
-    const i = await container.item(id, id).read();
-    if(i.statusCode === 200) return i.resource;
+async function getItemOrNullById<T>(container: Container, id: string, partitionKey?: string): Promise<T | null> {
+    /* 
+        If no partitionKey is provided, id will be used as partitionKey. 
+        If found in db removes all _ props (metadata from cosmos) and returns it, else returns null. 
+    */
+
+    if(!partitionKey) partitionKey = id;
+    const i = await container.item(id, partitionKey).read();
+    if (i.statusCode === 200) return Object.entries(i.resource).reduce((acc, [key, val]) => {
+        if (!key.startsWith("_")) return { ...acc, [key]: val }
+        else return acc
+    }, {}) as T;
     return null;
 }
 async function userIsInvited(client: CosmosClient, user: User) {
-    const foundUser = await getItemOrNullById(getContainer(client), user.email.toLowerCase());
-    if(foundUser.statusCode === 200) return true;
+    const foundUser = await getItemOrNullById<User>(getContainer(client), user.email.toLowerCase());
+    if (foundUser) return true;
     return false;
 }
 
 export async function Authenticate(request: HttpRequest) {
     const cosmosClient = await getCosmosClient();
     const accessToken = request.headers.get(TOKENHEADERNAME)
-    if(!accessToken) return {
+    if (!accessToken) return {
         success: false as const,
-        data: { status: 401, body: "Access token is missing."} as HttpResponseInit 
+        data: { status: 401, body: "Access token is missing." } as HttpResponseInit
     }
 
     const user = await getUserInfoFromIssuer(accessToken);
     const isInvited = await userIsInvited(cosmosClient, user);
-    if(!isInvited) return {
+    if (!isInvited) return {
         success: false as const,
-        data: {status: 403, body: "Not invited"} as HttpResponseInit
+        data: { status: 403, body: "Not invited" } as HttpResponseInit
     }
 
-    return {success: true as const, data: user};
+    return { success: true as const, data: user };
 }
